a98e7c8c946d74345c7226c5d64839eaf23fd58d
Rajsoni.github.io <raj.soni@rungta.org>
2025-12-26 19:51:35 +0530

    Add files via upload
    PRACTICAL_ASSIGNEMENT-3-PFC

---
+++ Practical3_3Assignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    printf("%d", factorial(num));
+    return 0;
+}
